9785a967af83c29ff81164a65e734b8f45532f84
FoziaAkhtar <fozia.shah0001@gmail.com>
2026-06-28 18:55:30 -0400

    Initial Commit

---
+++ iOSApp4-StudyPlannerPro/iOSApp4_StudyPlannerProApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSApp4_StudyPlannerProApp.swift
+//  iOSApp4-StudyPlannerPro
+//
+//  Created by Fozia on 2026-06-28.
+//
+
+import SwiftUI
+
+@main
+struct iOSApp4_StudyPlannerProApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
